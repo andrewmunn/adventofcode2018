83d1835da5e3fb1472c9a15aa468eb0c85bbb659
Andrew Munn <munn@stackstorage.co>
2018-12-06 09:46:05 -0800

    Reduce solution size for day 6 part 2:

--- src/main/kotlin/munn/adventofcode/day6/day6_part2.kt
+++ src/main/kotlin/munn/adventofcode/day6/day6_part2.kt
@@ -3,26 +3,17 @@ package munn.adventofcode.day6
 import java.io.File
 
 fun main(args: Array<String>) {
-    val points = File(args.first()).readLines()
-        .map { it.split(',') }
+    val points = File(args.first()).readLines().map { it.split(',') }
         .map { Point(it.first().trim().toInt(), it.last().trim().toInt()) }
 
-    val maxX = points.maxBy { it.x }!!.x + 100
+    val maxX = points.maxBy { it.x }!!.x + 100 // add 100 to the edge in case valid area is outside
     val maxY = points.maxBy { it.y }!!.y + 100
 
-    val grid = Array(maxX) { IntArray(maxY) }
-
-    points.forEach { point ->
-        grid.indices.forEach { x ->
-            grid[x].indices.forEach { y ->
-                grid[x][y] += Math.abs(point.x - x) + Math.abs(point.y - y)
-            }
-        }
+    val grid = IntArray(maxX * maxY) { i ->
+        val x = i / maxY
+        val y = i % maxY
+        points.sumBy { point -> Math.abs(point.x - x) + Math.abs(point.y - y) }
     }
 
-    val valid = grid.flatMap { it.toList() }
-        .filter { it < 10000 }
-        .count()
-
-    println(valid)
+    println(grid.filter { it < 10000 }.count())
 }
