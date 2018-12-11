f488fd9caa9ec4d09962b171934b6685b78ab520
Andrew Munn <munn@stackstorage.co>
2018-12-11 09:36:49 -0800

    Day 11 solutions

---
+++ src/main/kotlin/munn/adventofcode/day11/day11_part1.kt
@@ -0,0 +1,30 @@
+package munn.adventofcode.day11
+
+import munn.adventofcode.day6.Point
+
+fun main(args: Array<String>) {
+    val input = 5719L
+    val grid = Array(300) { x ->
+        IntArray(300) { y ->
+            val rackId = (x + 1) + 10L
+            (((rackId * (y + 1) + input) * rackId).toString().reversed().getOrNull(2)?.toInt() ?: 0) - 5
+        }
+    }
+
+    var max = Int.MIN_VALUE
+    var maxPoint = Point(0, 0)
+    grid.dropLast(2).indices.forEach { x ->
+        grid[x].dropLast(2).indices.forEach { y ->
+            val level = grid[x][y] + grid[x + 1][y] + grid[x + 2][y] +
+                    grid[x][y + 1] + grid[x + 1][y + 1] + grid[x + 2][y + 1] +
+                    grid[x][y + 2] + grid[x + 1][y + 2] + grid[x + 2][y + 2]
+            if (level > max) {
+                max = level
+                maxPoint = Point(x + 1, y + 1)
+            }
+        }
+    }
+
+    println(maxPoint)
+    println(max)
+}
---
+++ src/main/kotlin/munn/adventofcode/day11/day11_part2.kt
@@ -0,0 +1,40 @@
+package munn.adventofcode.day11
+
+import munn.adventofcode.day6.Point
+
+fun main(args: Array<String>) {
+    val input = 5719L
+    val grid = Array(300) { x ->
+        IntArray(300) { y ->
+            val rackId = (x + 1) + 10L
+            val value = (((rackId * (y + 1) + input) * rackId).toString().toInt() / 100 % 10) - 5
+            println(value)
+            value
+        }
+    }
+
+    var max = Int.MIN_VALUE
+    var maxPoint = Pair(Point(0, 0), 0)
+    (1..300).forEach { size ->
+        grid.dropLast(size - 1).indices.forEach { x ->
+            grid[x].dropLast(size - 1).indices.forEach { y ->
+                var level = 0
+                (0 until size).forEach { dx ->
+                    (0 until size).forEach { dy ->
+                        level += grid[x + dx][y + dy]
+                    }
+                }
+                if (level > max) {
+                    max = level
+                    maxPoint = Point(x + 1, y + 1) to size
+                }
+            }
+        }
+        println("Completed size: $size")
+        println("max so far: $maxPoint with $max")
+    }
+
+
+    println(maxPoint)
+    println(max)
+}
