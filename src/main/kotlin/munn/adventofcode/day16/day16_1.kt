268be9767dd3fb8e8944140385e4d81096e04c0a
Andrew Munn <munn@stackstorage.co>
2018-12-16 16:21:11 -0800

    simplify part 16

--- src/main/kotlin/munn/adventofcode/day16/day16_1.kt
+++ src/main/kotlin/munn/adventofcode/day16/day16_1.kt
@@ -5,32 +5,28 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val registers = mutableListOf(0, 0, 0, 0)
-    val instructions = createInstructions(registers)
+    val instructions = createInstructions(registers).map { it.second }
 
     val opcodeMap = mutableMapOf<Int, Instruction>()
-    val sampleData = File(args.first()).readLines().chunked(4).toMutableList()
-
-    var i = 0
-    while (opcodeMap.size < 16) {
-        val data = sampleData[i % sampleData.size]
+    File(args.first()).readLines().chunked(4).forEach { data ->
         val before = data[0].substringAfter("[").dropLast(1).split(",").map { it.trim().toInt() }
         val input = data[1].split(" ").map { it.trim().toInt() }
         val after = data[2].substringAfter("[").dropLast(1).split(",").map { it.trim().toInt() }
         val opcodeCount = instructions
-            .filter { (_, inst) ->
+            .filterNot { it in opcodeMap.values }
+            .filter { inst ->
                 registers.clear()
                 registers.addAll(before)
                 registers[input[3]] = inst(input[1], input[2])
                 registers == after
             }
-            .map { it.second }
-            .filterNot { it in opcodeMap.values }
         if (opcodeCount.size == 1) {
             opcodeMap[input[0]] = opcodeCount.single()
             println("Found opcode ${input[0]}")
         }
-        i++
     }
+
+    check(opcodeMap.size == 16)
 
     val codes = File(args[1]).readLines().map { it.split(" ").map { it.trim().toInt() } }
 
